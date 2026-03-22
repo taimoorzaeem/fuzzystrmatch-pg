053761e4b45efe5b9eea6f83f12cf7927d7bb2fa
Taimoor Zaeem <taimoorzaeem@gmail.com>
2026-03-22 11:50:51 +0500

    test: add test suite and add levenshtein distance tests
    Also added to CI.
    
    Signed-off-by: Taimoor Zaeem <taimoorzaeem@gmail.com>

---
+++ src/Data/FuzzyStrMatch.hs
@@ -0,0 +1,16 @@
+{- |
+Module      : Data.FuzzyStrMatch
+Description : Export all string matching functions
+Copyright   : (c) 2026 Taimoor Zaeem
+License     : MIT
+Maintainer  : Taimoor Zaeem <taimoorzaeem@gmail.com>
+Stability   : Experimental
+Portability : Portable
+-}
+module Data.FuzzyStrMatch
+  (
+    module Data.FuzzyStrMatch.Levenshtein
+  )
+  where
+
+import Data.FuzzyStrMatch.Levenshtein
