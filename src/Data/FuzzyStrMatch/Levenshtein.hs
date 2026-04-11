830bc71e5336bcbccfa5d33a79ae9886adb66d70
Taimoor Zaeem <taimoorzaeem@gmail.com>
2026-04-11 22:45:45 +0500

    add: calculate levenshtein distance with max distance bound
    Signed-off-by: Taimoor Zaeem <taimoorzaeem@gmail.com>

--- src/Data/FuzzyStrMatch/Levenshtein.hs
+++ src/Data/FuzzyStrMatch/Levenshtein.hs
@@ -57,19 +57,32 @@ data LState s = LState {
   , insCost :: Int
   , delCost :: Int
   , subCost :: Int
+  , maxD    :: Maybe Int
 }
 
 -- | Initialize the state
-initLState :: Text -> Text -> Int -> Int -> Int -> ST s (LState s)
-initLState s t ins del sub = do
+initLState :: Text -> Text -> Int -> Int -> Int -> Maybe Int -> ST s (LState s)
+initLState s t ins del sub maxD = do
   let sLen = T.length s
       tLen = T.length t
-  prev <- MVU.new $ tLen + 1
+      -- We always keep the larger string on the x-axis, helpes implementation
+      -- For costs, we have to switch ins and del cost when source >= target
+      sourceLenGreater = sLen >= tLen
+      (s',t',sLen',tLen',ins',del') =
+        if sourceLenGreater
+          then (t,s,tLen,sLen,del,ins)
+          else (s,t,sLen,tLen,ins,del)
+
+  prev <- MVU.new $ tLen' + 1
   -- Init: [0,1,2,..,tLen] for default 1 cost of insertion
   -- Init: [0,2,4,..,tLen * 2] for cost of insertion equal 2 and so on
-  mapFromUpto 0 tLen (\i -> MVU.write prev i (i * ins))
-  curr <- MVU.new $ tLen + 1
-  return $ LState s t sLen tLen prev curr ins del sub
+  _ <- mapFromUpto 0 tLen' (\i -> do
+    MVU.write prev i (i * ins')
+    return False
+    )
+
+  curr <- MVU.new $ tLen' + 1
+  return $ LState s' t' sLen' tLen' prev curr ins' del' sub maxD
 
 -- Calculation Matrix For Wagner-Fisher algorithm:
 -- ==============================================
@@ -117,38 +130,31 @@ levenshtein source target = levenshteinWithCosts source target 1 1 1
 levenshteinWithCosts :: Text -> Text -> Int -> Int -> Int -> Int
 levenshteinWithCosts s t ins del sub = runST $ do
 
-  lState@LState{..} <- initLState s t ins del sub
-  runLevenshtein lState
+  lState@LState{..} <- initLState s t ins del sub Nothing
+  _ <- runLevenshtein lState
 
   MVU.read prev tLen -- The last one is the answer at target length position
 
--- Threshold Optimization:
---  https://en.wikipedia.org/wiki/Wagner-Fischer_algorithm#Possible_modifications
-
-levenshteinLessEqual :: Text -> Text -> Int -> Int
-levenshteinLessEqual _ _ _ = 0
-
-levenshteinLessEqualWithCosts :: Text -> Text -> Int -> Int -> Int -> Int -> Int
-levenshteinLessEqualWithCosts _ _ _ _ _ _ = 0
-
 -- | Run Levenshtein algorithm
-runLevenshtein :: LState s -> ST s ()
-runLevenshtein lState@LState{..} =
-  mapFromUpto 0 (sLen - 1) $ calculateCurrentCostAndWrite lState
+runLevenshtein :: LState s -> ST s Bool
+runLevenshtein lState@LState{..} = do
+  exit <- mapFromUpto 0 (sLen - 1) $ calculateCurrentCostAndWrite lState
+  if exit then return True else return False
 
 -- | Calculate cost for all cells in the current row
-calculateCurrentCostAndWrite :: LState s -> (Int -> ST s ())
+calculateCurrentCostAndWrite :: LState s -> (Int -> ST s Bool)
 calculateCurrentCostAndWrite lState@LState{..} i = do
   -- Init: delete cost, goes like 1,2,3... for default 1 cost of deletion
   -- Init: delete cost, goes like 2,4,6... for when cost of deletion is 2 and so on
   MVU.write curr 0 ((i + 1) * delCost)
-  mapFromUpto 0 (tLen - 1) $ calculateCurrentCellCostAndWrite lState i
+  exit <- mapFromUpto 0 (tLen - 1) $ calculateCurrentCellCostAndWrite lState i
   MVU.copy prev curr -- Copy current to previous before next iteration
+  if exit then return True else return False
 
 -- | Return closure/function/handler to compute the values in a row
 --   This reads the current cell and write the minimum cost
 --   after calculating insert cost, del cost and sub cost
-calculateCurrentCellCostAndWrite :: LState s -> Int -> (Int -> ST s ())
+calculateCurrentCellCostAndWrite :: LState s -> Int -> (Int -> ST s Bool)
 calculateCurrentCellCostAndWrite LState{..} i j = do
   subCost' <- MVU.read prev j
   insCost' <- MVU.read prev (j + 1)
@@ -164,11 +170,68 @@ calculateCurrentCellCostAndWrite LState{..} i j = do
 
   MVU.write curr (j + 1) minCost
 
+  return $ case maxD of
+    Nothing -> False
+    Just n  -> minCost > n && i == (j+1) -- if we are on the diagonal and the minCost > maxD, we break
+
 -- | Map over the vector from index i to j and apply f on i
-mapFromUpto :: Int -> Int -> (Int -> ST s ()) -> ST s ()
+mapFromUpto :: Int -> Int -> (Int -> ST s Bool) -> ST s Bool
 -- "s" is the type signature is a "Phantom Type" which is needed by GHC
 -- state monad for type checking reasons. At runtime, "s" is nothing. So,
 -- dont' worry about it too much
 mapFromUpto i j f
-  | i > j     = return ()
-  | otherwise = f i >> mapFromUpto (i+1) j f
+  | i > j     = return False
+  | otherwise = do
+    exit <- f i
+    if exit then return True else mapFromUpto (i+1) j f
+
+
+-- ========================
+-- Threshold Optimization:
+--  https://en.wikipedia.org/wiki/Wagner-Fischer_algorithm#Possible_modifications
+-- =========================
+levenshteinLessEqual :: Text -> Text -> Int -> Maybe Int
+levenshteinLessEqual source target = levenshteinLessEqualWithCosts source target 1 1 1
+
+levenshteinLessEqualWithCosts :: Text -> Text -> Int -> Int -> Int -> Int -> Maybe Int
+levenshteinLessEqualWithCosts s t ins del sub maxD' = runST $ do
+
+  lState@LState{..} <- initLState s t ins del sub (Just maxD')
+  exit <- runLevenshtein lState
+
+  if exit -- If exit if True, that means we have broken early, return Nothing
+    then return Nothing
+    else do
+      -- An edge case we need to check, if the iterations are complete,
+      -- we calculate the cost all the way for last row and then check
+      -- if this is greater than maxD
+      --        | +---+---+---+---+---+---+---+
+      --        v | S | I | T | T | I | N | G |
+      --      +---+---+---+---+---+---+---+---+
+      --      | 0 |   |   |   |   |   |   |   |
+      --  +---|---+---+---+---+---+---+---+---+
+      --  | K | 1 | 1 |   |   |   |   |   |   |
+      --  +---|---+---+---+---+---+---+---+---+
+      --  | I | 2 | 2 | 1 | 2 | 3 | 4 | 5 | 6 | <- the diagnoal ends with cost 1
+      --  +---|---+---+---+---+---+---+---+---+    but inserstion costs are added
+      --
+      -- Transpose case for this:
+      --        | +---+---+---+
+      --        v | S | I | T | -- We DON'T need to implement this case,
+      --      +---+---+---+---+ -- because we always choose the longer string
+      --      | 0 |   |   |   | -- to be target INTERNALLY.
+      --  +---|---+---+---+---+
+      --  | K | 1 | 1 |   |   |
+      --  +---|---+---+---+---+
+      --  | I | 2 | 2 | 1 | 2 |
+      --  +---|---+---+---+---+
+      --  | T | 3 | 3 | 2 | 1 | <- exit here, no need to calculate further
+      --  +---|---+---+---+---+
+      --  | T | 4 | 4 | 3 | 2 |
+      --  +---|---+---+---+---+
+      --  | E | 5 | 5 | 4 | 3 |
+      --  +---|---+---+---+---+
+      --  | N | 6 | 6 | 5 | 4 |  <-- This is our answer, but we need to exit early
+      --  +---|---+---+---+---+
+      d <- MVU.read prev tLen
+      return $ if d > maxD' then Nothing else Just d
--- test/spec/Main.hs
+++ test/spec/Main.hs
@@ -2,10 +2,12 @@ module Main
   ( main )
   where
 
+import qualified Criterion.Main as C
 import qualified Test.Hspec as HS
 
 import qualified LevenshteinSpec
 
+import Data.FuzzyStrMatch.Levenshtein
 import Test.Hspec.Runner
 import Prelude
 
@@ -22,3 +24,22 @@ main = do
   
   putStrLn $ "Total tests: " ++ show (summaryExamples summary)
   putStrLn $ "Failures: "    ++ show (summaryFailures summary)
+
+  -- =========
+  -- Benchmark:
+  -- =========
+  -- TODO: Use more examples, for now this will do
+  let source = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      target = "abababababababababababababababababababababababababababab"
+
+  -- We evaluate to normal form, because we want full evaluation
+  C.defaultMain
+    [
+      -- According to bench mark, a full distance calculation time = ~225 us
+      -- In comparison, bounded calculation time = ~50 us
+
+      -- Hence proving that this exits early as soon as the bound hits, I
+      -- believe the difference would be even larger for bigger strings
+      C.bench "levenshtein" $ C.nf (levenshtein source) target
+    , C.bench "levenshteinLessEqual" $ C.nf (levenshteinLessEqual source target) 3
+    ]
